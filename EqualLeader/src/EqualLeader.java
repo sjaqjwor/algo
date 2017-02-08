48c4286d84cb4d3dd4752818205ce61235ff5f3d
sjaqjwor <lsklsk45@naver.com>
2017-02-08 20:08:55 +0900

    codiliy 8-2

--- EqualLeader/src/EqualLeader.java
+++ EqualLeader/src/EqualLeader.java
@@ -3,21 +3,15 @@ public class EqualLeader {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] arr = new int[] { 4, 3, 4, 4, 4, 2 };
+		int[] arr = new int[] {1, 2, 1, 1, 2, 1};
 		System.out.println(solution(arr));
 	}
 
 	static int solution(int[] arr) {
 		stack st = new stack();
-		stack st1 = new stack();
 		int[] pre_count = new int[arr.length];
-		int[] pos_count = new int[arr.length];
 		int same = 0;
-		int same_2 = 0;
-		int max_1 = 0;
-		int num = 0;
-		for (int a = 0; a < arr.length-1; a++) {
-			for (int c = 0; c <= a; c++) {
+			for (int c = 0; c < arr.length; c++) {
 				if (st.em()) {
 					st.push(pre_count, arr[c]);
 					same++;
@@ -33,47 +27,38 @@ public class EqualLeader {
 					}
 				}
 			}
-			for (int b = a + 1; b < arr.length; b++) {
-				if (st1.em()) {
-					st1.push(pos_count, arr[b]);
-					same_2++;
-				} else if (st1.pop(pos_count) == arr[b]) {
-					st1.push(pos_count, arr[b]);
-					same_2++;
-				} else {
-					same_2--;
-					if (same_2 == 0) {
-						st1.top = 0;
-						st1.push(pos_count, arr[b]);
-						same_2++;
-					}
-				}
+		int []post = new int[arr.length];
+		int []back = new int[arr.length];
+		same=0;
+		for(int a=0;a<arr.length;a++){
+			if(arr[a]==st.pop(pre_count)){
+				same++;
+				post[a]=same;
 			}
-			same=0;
-			for(int a1=0;a1<=a;a1++){
-				if(arr[a1]==st.pop(pre_count)){
-					same++;
-				}
+			else{
+				post[a]=same;
 			}
-			same_2=0;
-			for(int a1=a+1;a1<arr.length;a1++){
-				if(arr[a1]==st1.pop(pos_count)){
-					same_2++;
-				}
-			}
-			if(a==0||(same>(a+1)/2&&same_2>((arr.length-1)-a)/2)){
-				if(st.pop(pre_count)==st1.pop(pos_count)){
-					num++;
-				}
-			}
-			st1.top = 0;
-			st.top = 0;
-			same=0;
-			same_2=0;
 		}
-
-		return num;
-
+		same=0;
+		for(int a=0;a<arr.length;a++){
+			if(arr[a]!=st.pop(pre_count)){
+				same++;
+				back[a]=same;
+			}else{
+				back[a]=same;
+			}
+		}
+		same=0;
+		//post[arr.length-1]-post[a] 지금자리에서 뒤에 가장많은수 갯수
+		//back[arr.length-1]-back[a] 지금 자리에서 뒤에 가장많은수가 아닌 다른수 갯수
+		for(int a=0;a<arr.length;a++){
+			if(post[a]>back[a]&&post[arr.length-1]-post[a]>back[arr.length-1]-back[a]){
+				same++;
+			}
+		}
+		
+		
+		return same;
 	}
 }
 
